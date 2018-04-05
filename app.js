f61443eaf979cea808c8f03dae516da5a87225ce
James Hughes <heezmagnif@gmail.com>
2018-04-05 15:58:35 -0400

    Adds mongodb and db connection

--- app.js
+++ app.js
@@ -1,34 +1,70 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    { name: "Salmon Creek", image: "https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/camping-expert-north-cascade-range-washington-lightweight-tent-0514.jpg?itok=u5IHDgjl" },
-    { name: "Granite Creek", image: "https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/camping-with-kids-lake-alpine-sierra-nevada-family-night-out-0514.jpg?itok=ocbTWgVV" },
-    { name: "Mule River", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg" }
-]
+//Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//         name: "Granite Creek",
+//         image: "https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/camping-with-kids-lake-alpine-sierra-nevada-family-night-out-0514.jpg?itok=ocbTWgVV"
+//     },
+//     function(err, campground) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Newly created campground");
+//             console.log(campground);
+//         }
+//     });
+
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-
-
-    res.render("campgrounds", { campgrounds: campgrounds });
+    // get all campgrounds from db
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", { campgrounds: allCampgrounds });
+        }
+    });
 });
+
+
+
+
 
 app.post("/campgrounds", function(req, res) {
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = { name: name, image: image }
-    campgrounds.push(newCampground);
-    // redirect back to campgrounds page
-    res.redirect("/campgrounds");
+        // create a new campground and save to db
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+
+        } else {
+
+            // redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res) {
