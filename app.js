58e95a6e74873b20e6b2059af35cea5ce3beced0
James Hughes <heezmagnif@gmail.com>
2018-04-09 15:44:42 -0400

    Adds styling

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ var express = require("express"),
     seedDB = require("./seeds");
 
 
-seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"))
+seedDB();
 
 
 app.get("/", function(req, res) {
