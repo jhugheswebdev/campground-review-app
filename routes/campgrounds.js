2549e5acada5b6276b5606f8bc05a4125b388336
James Hughes <heezmagnif@gmail.com>
2018-04-10 14:15:45 -0400

    Adds delete function and button styling

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -78,7 +78,17 @@ router.put("/:id", function(req, res) {
             res.redirect("/campgrounds/" + req.params.id);
         }
     });
-    // redirect somewhere(show page)
+});
+
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", function(req, res) {
+    Campground.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 
