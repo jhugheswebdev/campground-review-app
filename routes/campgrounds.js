c01e1bedefc7765153afa3c5e175e03b7c4e5a55
James Hughes <heezmagnif@gmail.com>
2018-04-10 14:02:40 -0400

    Adds Edit and Update functions

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,6 +57,31 @@ router.get("/:id", function(req, res) {
     });
 });
 
+// EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.render("campgrounds/edit", { campground: foundCampground });
+        }
+    });
+});
+
+// UPDATE CAMPGROUND ROUTE
+router.put("/:id", function(req, res) {
+    // find and update correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+    // redirect somewhere(show page)
+});
+
+
 // Middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
