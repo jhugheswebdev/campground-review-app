29e559453da94df0ea1de182b49acf603db259fb
James Hughes <heezmagnif@gmail.com>
2018-04-11 16:17:03 -0400

    Adds UI improvements

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,22 +11,23 @@ router.get("/", function(req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds/index", { campgrounds: allCampgrounds, currentUser: req.user });
+            res.render("campgrounds/index", { campgrounds: allCampgrounds, page: "campgrounds" });
         }
-    })
+    });
 });
 
 //CREATE - add new campground to DB
 router.post("/", middleware.isLoggedIn, function(req, res) {
     // get data from form and add to campgrounds array
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = { name: name, image: image, description: desc, author: author }
+    var newCampground = { name: name, price: price, image: image, description: desc, author: author }
         // create a new campground and save to db
     Campground.create(newCampground, function(err, newlyCreated) {
         if (err) {
