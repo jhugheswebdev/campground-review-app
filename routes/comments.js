41a634b45f8a28edc26d6768395a9d9a6a2203d5
James Hughes <heezmagnif@gmail.com>
2018-04-11 15:20:26 -0400

    Fixes issues and bugs that may crash or break application in production

--- routes/comments.js
+++ routes/comments.js
@@ -48,14 +48,19 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
 
 // COMMENTS EDIT ROUTE
 router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res) {
-    Comment.findById(req.params.comment_id, function(err, foundComment) {
-        if (err) {
-            res.redirect("back");
-        } else {
-            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
-
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err || !foundCampground) {
+            req.flash("error", "No campground found");
+            return res.redirect("back");
         }
-    })
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+            }
+        });
+    });
 });
 
 // COMMENT UPDATE
@@ -80,8 +85,5 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
         }
     });
 });
-
-
-
 
 module.exports = router;
