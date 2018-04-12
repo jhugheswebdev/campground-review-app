b97964dad3482d35d9c2092361dc45952687f2d0
James Hughes <heezmagnif@gmail.com>
2018-04-12 13:23:24 -0400

    Adds user profiles

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Campground = require("../models/campground");
 
 // root route
 router.get("/", function(req, res) {
@@ -15,8 +16,15 @@ router.get("/register", function(req, res) {
 
 //handle signup logic
 router.post("/register", function(req, res) {
-    var newUser = new User({ username: req.body.username });
-    if (req.body.adminCode === "123") {
+    var newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+    });
+
+    if (req.body.adminCode === "123j") {
         newUser.isAdmin = true;
     }
     User.register(newUser, req.body.password, function(err, user) {
@@ -52,4 +60,20 @@ router.get("/logout", function(req, res) {
     res.redirect("/campgrounds");
 });
 
+// USER PROFILE
+router.get("/users/:id", function(req, res) {
+    User.findById(req.params.id, function(err, foundUser) {
+        if (err) {
+            req.flash("error", "Something went wrong.");
+            return res.redirect("/");
+        }
+        Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds) {
+            if (err) {
+                req.flash("error", "Something went wrong.");
+                return res.redirect("/");
+            }
+            res.render("users/show", { user: foundUser, campgrounds: campgrounds });
+        })
+    });
+});
 module.exports = router;
