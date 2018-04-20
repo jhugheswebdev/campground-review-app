005952364220dd0ad71ac9a84973f331f5c18ce4
James Hughes <heezmagnif@gmail.com>
2018-04-20 12:12:46 -0400

    deploy code

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 // ===========================================
 // LOCAL SERVER FOR DEVELOPMENT
 // ===========================================
-app.listen(3000, process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The YelpCamp Server Has Started!");
 });
